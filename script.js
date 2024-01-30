10fa596097f4ac00896043747c3b04466034c5e3
Felipe silva <125931907+felipegsilva11@users.noreply.github.com>
2024-01-30 10:42:01 -0300

    atualizando-paly

---
+++ script.js
@@ -0,0 +1,10 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+const numeroCapitulos = 10;
+
+function tocarFaixa() {
+    audioCapitulo.play();
+}
+
+botaoPlayPause.addEventListener('click', tocarFaixa);
